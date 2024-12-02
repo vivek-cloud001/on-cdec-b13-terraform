5a5d572d21bfaf072b57930450e7d9ac191736f2
Vivek <viveknimje001@gmail.com>
2024-12-02 19:51:23 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "instance_1" {
     ami = "ami-0c80e2b6ccb9ad6d1"
-    instance_yype = "t2.micro"
+    instance_type = "t2.micro"
     key_name = "new-key"
     security_groups = ["defaults"]
 }
